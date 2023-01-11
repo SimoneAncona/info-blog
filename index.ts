ffd20782f4b9febbd0180198013184431ebb6ab8
simoneancona04 <simone.ancona.2004@calvino.edu.it>
2023-01-11 12:06:29 +0100

    init

---
+++ index.ts
@@ -0,0 +1,6 @@
+import * as mysql from "mysql2";
+import * as express from "express";
+import * as cors from "cors";
+import * as fs from "fs";
+import * as bodyParser from "body-parser";
+import * as dotenv from "dotenv";
