f2ffe9bb294088a3e9f4bbca2a40412f7ca926ac
SimoneAncona <simone.ancona00@gmail.com>
2023-01-16 10:16:27 +0100

    little changes

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("ciao");
+"use strict";
+exports.__esModule = true;
+var express = require("express");
+var dotenv = require("dotenv");
+var path = require("path");
+var app = express();
+dotenv.config();
+var port = process.env.PORT || 3000;
+function sendQuery(query) {
+    return new Promise(function (resolve, reject) { });
+}
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "./pages/index.html"));
+});
+app.get("/resources/css/", function (req, res) {
+    res.sendFile(path.join(__dirname, "./style/main.css"));
+});
+app.listen(port, function () {
+    console.log("listening on port %d", port);
+});
--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import * as cors from "cors";
 import * as fs from "fs";
 import * as bodyParser from "body-parser";
 import * as dotenv from "dotenv";
+import * as path from "path";
 
 const app = express();
 dotenv.config();
@@ -15,10 +16,14 @@ function sendQuery(query: string): Promise<any> {
 
 // page request
 app.get("/", (req, res) => {
-
+    res.sendFile(path.join(__dirname, "./pages/index.html"));
 });
 
 // get css
 app.get("/resources/css/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./style/main.css"));
+});
 
+app.listen(port, () => {
+    console.log("listening on port %d", port);
 });
